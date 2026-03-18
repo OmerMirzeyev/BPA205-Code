5d55b5492bb3509d091e86e2c0c45005b315437f
Omer2121212121 <mirzeyevomer1@gmail.com>
2026-03-18 12:30:06 +0400

    Add files via upload

---
+++ Ders3/Ders3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ders3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Task1/Task1/Moduls/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task1.Moduls
+{
+    //public class Book : Product
+    //{
+    //    public string Author;
+    //    public void ShowInfo()
+    //    {
+    //        base.ShowInfo();
+    //        Console.WriteLine($"Müellifi: {Author}");
+    //    }
+    //}
+}
