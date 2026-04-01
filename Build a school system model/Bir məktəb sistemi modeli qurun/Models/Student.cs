e00f8fa1ffa3015bd9fcd264233b657f7e38f694
OmerMirzeyev <mirzeyevomer1@gmail.com>
2026-04-01 18:19:13 +0400

    Build a school system model

---
+++ Build a school system model/Bir məktəb sistemi modeli qurun/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bir_məktəb_sistemi_modeli_qurun_.Models
+{
+    public class Student
+    {
+        public static int Id { get; set; } = 0;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public const string SchoolName = "Code Academy";
+        public static int studentCount;
+        public readonly string studentCode;
+        public int StudentId;
+        public Student(string name, string surname, int age)
+        {
+            Id++;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            studentCount++;
+            studentCode = $"STU{Id:0000}";
+            StudentId = Id;
+        }
+        public void GetStudentCount()
+        {
+            Console.WriteLine($"Total number of students: {studentCount}");
+        }  
+            public void GetInfo()
+            {
+                Console.WriteLine($"Student Code: {studentCode},Id: {StudentId}, Name: {Name} {Surname}, Age: {Age}, School: {SchoolName}");
+            }
+    }
+}
---
+++ Build a school system model/Bir məktəb sistemi modeli qurun/Program.cs
@@ -0,0 +1,19 @@
+﻿using Bir_məktəb_sistemi_modeli_qurun_.Models;
+
+namespace Bir_məktəb_sistemi_modeli_qurun_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Omer", "Mirzeyev", 20);
+            Student student2 = new Student("Kenan", "Murselizade", 22);
+            Student student3 = new Student("Rovsen", "Mammadov", 19);
+            student1.GetInfo();
+            student2.GetInfo();
+            student3.GetInfo();
+            Console.WriteLine("--------------------------------------");
+            student1.GetStudentCount();
+        }
+    }
+}
