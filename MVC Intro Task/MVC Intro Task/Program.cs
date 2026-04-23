89293c1397c1d20613f04bd359217ea1da67c55f
OmerMirzeyev <mirzeyevomer1@gmail.com>
2026-04-24 02:07:20 +0400

    MVC Intro Task

---
+++ MVC Intro Task/MVC Intro Task/Program.cs
@@ -0,0 +1,21 @@
+namespace MVC_Intro_Task
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseRouting();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
