996df4cac789df862b3418d3fbef70b00fc91f83
Omer2121212121 <mirzeyevomer1@gmail.com>
2026-04-28 19:33:58 +0400

    Ef-coretask

---
+++ EF core task/Models/Base/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace EF_core_task.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ EF core task/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace EF_core_task.Models
+{
+    public class Product : BaseEntity
+    {
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+
+    }
+}
