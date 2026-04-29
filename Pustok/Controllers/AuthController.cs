0b524cc5a666edba8ba33aef32a0bfc4a88563c9
Omer2121212121 <mirzeyevomer1@gmail.com>
2026-04-29 17:00:53 +0400

    Pustok

---
+++ Pustok/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EF_core_task.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Pustok/Models/Base/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace EF_Core_Task.Models.Base
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
